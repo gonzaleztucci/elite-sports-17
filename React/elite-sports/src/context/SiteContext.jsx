6a9ac3d13e953285b110dcef24f51e2b5df2f655
gonzaleztucci <gonzaleztucci@gmail.com>
2022-04-08 18:42:04 +0200

    SiteContext file created

---
+++ React/elite-sports/src/context/SiteContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const SiteContext = createContext(); 
+
+export const SiteContextProvider = props => {
+    const [currentUsers, setCurrentUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [darkMode, setDarkMode] = useState(false);
+
+
+    return (
+        <SiteContext.Provider value = {{currentUsers, setCurrentUsers, darkMode, setDarkMode, selectedUser, setSelectedUser}}>
+            {props.children}
+        </SiteContext.Provider>
+    
+    );
+}
+
