2725dcd449de868fba725655bccbd070761cbcba
gonzaleztucci <gonzaleztucci@gmail.com>
2022-04-11 10:51:37 +0200

    table populated

--- React/elite-sports/src/components/UserList.js
+++ React/elite-sports/src/components/UserList.js
@@ -38,26 +38,65 @@ const UserList = () => {
         load();
       }, []);
 
+      const handleUserSelect = (id) => {
+          alert('click' + id);
+      }
+
   return (
-      <Container>
-        <TableHeader>
-            <HeaderText>Picture</HeaderText>
-            <HeaderText>Name</HeaderText>
-            <HeaderText>Age</HeaderText>
-            <HeaderText>Country</HeaderText>
-            <HeaderText>email</HeaderText>
-        </TableHeader>
-        {
-            currentUsers.map(user => <UserTile 
-                                        key={user.id}
-                                        picture = {user.picture.thumbnail}
-                                        name = {user.name}
-                                        age = {user.dob.age}
-                                        country = {user.location.country}
-                                        email = {user.email}
-                                         />)
-        }
-      </Container>
+    //   <Container>
+    //     <TableHeader>
+    //         <HeaderText>Picture</HeaderText>
+    //         <HeaderText>Name</HeaderText>
+    //         <HeaderText>Age</HeaderText>
+    //         <HeaderText>Country</HeaderText>
+    //         <HeaderText>email</HeaderText>
+    //     </TableHeader>
+    //     {
+    //         currentUsers.map(user => <UserTile 
+    //                                     key={user.id}
+    //                                     picture = {user.picture.thumbnail}
+    //                                     name = {user.name}
+    //                                     age = {user.dob.age}
+    //                                     country = {user.location.country}
+    //                                     email = {user.email}
+    //                                     onClick = { () => handleUserSelect(user.id)}
+    //                                      />)
+    //     }
+    //   </Container>
+
+    <div className="container" style={{padding:"1.5rem"}} >
+    <div className='list-group'>
+        <table className="table table-hover table-light">
+            <thead className='thead-dark'>
+                <tr className="bg-primary">
+                    <th scope="col">Picture</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">email</th> 
+                </tr>
+            </thead>
+            <tbody>
+               {
+                    currentUsers.map(user => {
+                        console.log(user);
+                        return(
+                        <tr>
+                            <td><img src={user.picture.thumbnail} alt='user profile'/></td>
+                            <td>{`${user.name.first} ${user.name.last}`}</td>
+                            <td>{user.dob.age}</td>
+                            <td>{user.location.country}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        )
+                    })
+                } 
+            </tbody>
+     </table>
+    </div>
+
+</div>
+
     
   )
 }
